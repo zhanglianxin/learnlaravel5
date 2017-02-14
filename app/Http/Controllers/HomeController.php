10d92f2f832f1ca6f4c8fd4d4acd74ad24b97657
zhanglianxin <zhanglianxin1993@163.com>
2017-02-14 21:34:23 +0800

    Display all articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use App\Http\Requests;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home')->withArticles(Article::all());
     }
 }
