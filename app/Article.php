9de8484a17061ebf6d23c3cb6a4937a583968ce5
zhanglianxin <zhanglianxin1993@163.com>
2017-02-21 20:30:52 +0800

    建立 Model、生成 Migration、建立一对多关系

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    // 一对多关系
+    public function hasManyComments()
+    {
+        return $this->hasMany('App\Comment', 'article_id', 'id');
+    }
 }
